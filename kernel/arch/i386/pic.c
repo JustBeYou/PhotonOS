3a5ee4d5af9140a8811ff20520e68cb3979476ac
JustBeYou <mihailferaru2000@gmail.com>
2015-08-04 21:25:27 +0300

    Exceptions handling.

---
+++ kernel/arch/i386/pic.c
@@ -0,0 +1,31 @@
+#include <pic.h>
+#include <stdint.h>
+#include <io.h>
+
+void IRQ_set_mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);
+}
+
+void IRQ_clear_mask(unsigned char IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if(IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);
+}
