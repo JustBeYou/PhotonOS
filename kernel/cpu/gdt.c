057968fad17febadebd0ed972079bacbb725c268
JustBeYou <mihailferaru2000@gmail.com>
2015-08-04 17:11:27 +0300

    GDT installition procedure.

---
+++ kernel/cpu/gdt.c
@@ -0,0 +1,54 @@
+#include <io.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <gdt.h>
+#include <isr.h>
+#include <irq.h>
+#include <string.h> 
+
+void init_gdt()
+{
+   gdt_ptr.lim = (sizeof(gdt_entry_t) * 5) - 1;
+   gdt_ptr.base  = (uint32_t)&gdt_entries;
+
+   gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
+   gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
+   gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF); // Data segment
+   gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
+   gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
+
+   gdt_flush((uint32_t)&gdt_ptr);
+}
+
+void gdt_set_gate(int32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
+{
+   gdt_entries[num].base_low    = (base & 0xFFFF);
+   gdt_entries[num].base_mid = (base >> 16) & 0xFF;
+   gdt_entries[num].base_high   = (base >> 24) & 0xFF;
+
+   gdt_entries[num].lim_low   = (limit & 0xFFFF);
+   gdt_entries[num].granularity = (limit >> 16) & 0x0F;
+
+   gdt_entries[num].granularity |= gran & 0xF0;
+   gdt_entries[num].access      = access;
+}
+
+void write_tss(int32_t num, uint16_t ss0, uint32_t esp0)
+{
+	uint32_t base = (uint32_t) &tss_entry;
+	uint32_t limit = base + sizeof(tss_entry);
+   
+	gdt_set_gate(num, base, limit, 0xE9, 0x00);
+	memset(&tss_entry, 0, sizeof(tss_entry));
+
+	tss_entry.ss0  = ss0;
+	tss_entry.esp0 = esp0;
+
+	tss_entry.cs   = 0x0b;
+	tss_entry.ss = tss_entry.ds = tss_entry.es = tss_entry.fs = tss_entry.gs = 0x13;
+}
+
+void set_kernel_stack(uint32_t stack)
+{
+	tss_entry.esp0 = stack;
+}
