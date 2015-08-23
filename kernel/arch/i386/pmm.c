803207eb466cb7feaf86e02406543c2140987eef
JustBeYou <mihailferaru2000@gmail.com>
2015-08-23 21:43:48 +0300

    Deleted paging code and implemented a frame allocator.

---
+++ kernel/arch/i386/pmm.c
@@ -0,0 +1,54 @@
+/* Paging driver
+ *
+ * Copyright (c) 2015 Feraru Mihail (mihailferaru2000@gmail.com).
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ */
+
+#include <pmm.h>
+#include <stdint.h>
+#include <stddef.h>
+#include <io.h>
+#include <string.h>
+#include <system.h>
+
+uint32_t nframes;
+uint32_t *frames;
+uint32_t placement_addr = (uint32_t) &kernel_end;
+int paging_enabled = 0;
+
+void init_pmm(uint32_t mem_size)
+{
+    nframes = mem_size / FRAME_SIZE;
+    frames = kmalloc(sizeof(uint32_t) * nframes);
+    for (int i = 0; i < nframes; i++) {
+        free_frame(FRAME_ADDR_FROM_INDEX(i));
+    }
+}
+
+uint32_t find_frame()
+{
+   for (int i = 0; i < nframes; i++) {
+        if (!frames[i]) {
+	    uint32_t frame_addr = FRAME_ADDR_FROM_INDEX(i);
+            use_frame(frame_addr);
+	    return frame_addr;
+	}
+   }
+   printk("[ERROR] No free frames!");
+   keep_running();
+}
+
+void use_frame(uint32_t addr)
+{
+    int frame_index = FRAME_INDEX(addr);
+    frames[frame_index] = 1;
+}
+
+void free_frame(uint32_t addr)
+{
+    int frame_index = FRAME_INDEX(addr);
+    frames[frame_index] = 0;
+}
