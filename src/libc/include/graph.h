6fd03a7e88988b6d9b8b50a062c314078ef5882a
JustBeYou <mihailferaru2000@gmail.com>
2016-01-10 18:13:22 +0200

    Improved graphs module.

--- src/libc/include/graph.h
+++ src/libc/include/graph.h
@@ -11,7 +11,7 @@ typedef struct graph_node {
     union {
         int id;
         char *name;
-    }
+    };
     int visited;
     
     void *data;
@@ -22,6 +22,6 @@ graph_node_t *graph_create(int id, char *name, void *data);
 graph_node_t *graph_find(graph_node_t *base, int id, char *name);
 int graph_add_node(graph_node_t *parent, graph_node_t *child);
 int graph_is_reachable(graph_node_t *a, graph_node_t *b);
-void *graph_destroy(graph_node_t *graph);
+void graph_destroy(graph_node_t *graph);
 
 #endif
--- src/libc/phapi/graph.c
+++ src/libc/phapi/graph.c
@@ -4,4 +4,73 @@
 #include <string.h>
 #include <sys/cdefs.h>
 
+graph_node_t *graph_create(int id, char *name, void *data)
+{
+    graph_node_t *g = malloc(sizeof(graph_node_t));
+    
+    if ((id < 0 && name == NULL) || (id > -1 && name != NULL)) {
+        return NULL;
+    }
+    
+    if (id > -1) {
+        g->id = id;
+    }
+    
+    if (name != NULL) {
+        g->name = name;
+    }
+    
+    g->visited = 0;
+    g->data = data;
+    
+    g->nodes = malloc(sizeof(Llist_t));
+    g->nodes->prev = NULL;
+    g->nodes->next = NULL;
+    g->nodes->data = NULL;
+    
+    return g;
+}
 
+graph_node_t *graph_find(graph_node_t *base, int id, char *name)
+{
+    return NULL;
+}
+
+int graph_add_node(graph_node_t *parent, graph_node_t *child)
+{
+    if (parent->nodes->data == NULL) {
+        parent->nodes->data = (void*) child;
+        return 0;
+    } else {
+        Llist_t *node_list = parent->nodes;
+        while (node_list->next != NULL) {
+            node_list = node_list->next;
+        }
+        node_list->next = malloc(sizeof(Llist_t));
+        node_list->next->next = NULL;
+        node_list->next->prev = node_list;
+        node_list->next->data = (void*) child;
+        return 0;
+    }
+    
+    return 1;
+}
+
+int graph_is_reachable(graph_node_t *a, graph_node_t *b)
+{
+    Llist_t *node_list = a->nodes;
+    while (node_list != NULL) {
+        Llist_t *cmp_data = (Llist_t*) node_list->data;
+        if (cmp_data == b) {
+            return 1;
+        }
+        node_list = node_list->next;
+    }
+    
+    return 0;
+}
+
+void graph_destroy(graph_node_t *graph)
+{
+    // TODO: implement destroy
+}
