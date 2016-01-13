afbec0bc96aba766654e4dd25bbba1388d247fe1
JustBeYou <mihailferaru2000@gmail.com>
2016-01-13 18:41:02 +0200

    Added Photon File System structures.

--- src/kernel/include/vfs.h
+++ src/kernel/include/vfs.h
@@ -28,29 +28,28 @@
 
 #define MAX_KERNEL_FD 256
 
-struct fs_node;
+struct super_block;
+struct inode;
 struct dirent;
 struct stat;
-struct vfs_entry;
 
-typedef int (*fs_read_t)(struct fs_node *node, size_t sz, int n, char *buf);
-typedef int (*fs_write_t)(struct fs_node *node, size_t sz, int n, char *buf);
-typedef int (*fs_rewind_t)(struct fs_node *node);
-typedef int (*fs_open_t)(struct fs_node *node, uint32_t flags);
-typedef int (*fs_close_t)(struct fs_node *node);
-typedef struct dirent *(*fs_readdir_t)(struct fs_node *node);
-typedef struct fs_node *(*fs_finddir_t)(struct fs_node *node, char *name);
-typedef int (*fs_create_t)(struct fs_node *parent, char *name, uint32_t flags);
-typedef int (*fs_mkdir_t)(struct fs_node *parent, char *name, uint32_t flags);
-typedef int (*fs_ioctl_t)(struct fs_node *node, int req, void *argp);
-typedef size_t (*fs_getsize_t)(struct fs_node *node);
-typedef int (*fs_chmod_t)(struct fs_node *node, uint32_t mode);
-typedef int (*fs_unlink_t)(struct fs_node *node);
-typedef int (*fs_symlink_t)(struct fs_node *node, struct fs_node *link);
-typedef int (*fs_readlink_t)(struct fs_node *node);
+typedef int (*inode_read_t)(struct inode *node, size_t sz, int n, char *buf);
+typedef int (*inode_write_t)(struct inode *node, size_t sz, int n, char *buf);
+typedef int (*inode_rewind_t)(struct inode *node);
+typedef int (*inode_open_t)(struct inode *node, uint32_t flags);
+typedef int (*inode_close_t)(struct inode *node);
+typedef struct dirent *(*inode_readdir_t)(struct inode *node);
+typedef struct inode *(*inode_finddir_t)(struct inode *node, char *name);
+typedef int (*inode_create_t)(struct inode *parent, char *name, uint32_t flags);
+typedef int (*inode_mkdir_t)(struct inode *parent, char *name, uint32_t flags);
+typedef int (*inode_ioctl_t)(struct inode *node, int req, void *argp);
+typedef size_t (*inode_getsize_t)(struct inode *node);
+typedef int (*inode_chmod_t)(struct inode *node, uint32_t mode);
+typedef int (*inode_unlink_t)(struct inode *node);
+typedef int (*inode_symlink_t)(struct inode *node, struct inode *link);
+typedef int (*inode_readlink_t)(struct inode *node);
 
-typedef struct fs_node {
-    char name[256];       // Node name
+typedef struct inode {
     void *device;         // Device where node is found
     uint32_t mask;        // Node mask
     uint32_t uid;         // User ID
@@ -58,36 +57,52 @@ typedef struct fs_node {
     uint32_t flags;       // Node flags
     uint32_t inode;       // Node ID
     size_t length;        // Node content length
-    uint32_t impl;
+    uint32_t nblocks;     // Number of blocks
+    uint32_t block;       // Block size
     uint32_t open_flags;  // Flags for open
-    size_t count;         // Cursor position
+    uint32_t lock;        // Inode is locked or not
+    size_t offset;        // Cursor position
     
     uint32_t atime;       // Accessed time
     uint32_t mtime;       // Modified time
     uint32_t ctime;       // Created time
     
     // Functions
-    fs_read_t      read;
-    fs_write_t     write;
-    fs_rewind_t    rewind;
-    fs_open_t      open;
-    fs_close_t     close;
-    fs_readdir_t   readdir;
-    fs_finddir_t   finddir;
-    fs_create_t    create;
-    fs_mkdir_t     mkdir;
-    fs_ioctl_t     ioctl;
-    fs_getsize_t   getsize;
-    fs_chmod_t     chmod;
-    fs_unlink_t    unlink;
-    fs_symlink_t   symlink;
-    fs_readlink_t  readlink;
+    inode_read_t      read;
+    inode_write_t     write;
+    inode_rewind_t    rewind;
+    inode_open_t      open;
+    inode_close_t     close;
+    inode_readdir_t   readdir;
+    inode_finddir_t   finddir;
+    inode_create_t    create;
+    inode_mkdir_t     mkdir;
+    inode_ioctl_t     ioctl;
+    inode_getsize_t   getsize;
+    inode_chmod_t     chmod;
+    inode_unlink_t    unlink;
+    inode_symlink_t   symlink;
+    inode_readlink_t  readlink;
     
     // Link and reference
-    struct fs_node *link;
+    struct inode *link;
     int32_t refcount;
     uint32_t nlink;
-} fs_node_t;
+} inode_t;
+
+typedef struct {
+    void *to_be_implemented; // TODO: implement this
+} file_system_type;
+
+typedef struct super_block {
+    void *device;
+    inode_t *inode;
+    uint32_t block;
+    file_system_type *fs_type;
+    void *fs_data;
+    
+    // TODO: add operations
+} super_block_t;
 
 struct dirent {
     uint32_t ino;
@@ -111,27 +126,51 @@ struct stat {
     uint32_t __unused3;
 };
 
-typedef struct vfs_entry {
-    char *name;
-    fs_node_t *node;
-} vfs_entry_t;
-
 void init_vfs();
-int fs_read(struct fs_node **node, size_t sz, int n, char *buf);
-int fs_get_fd(fs_node_t *node); // Not in struct.
-int fs_write(struct fs_node **node, size_t sz, int n, char *buf);
-int fs_rewind(struct fs_node *node);
-int fs_open(struct fs_node *node, uint32_t flags);
-int fs_close(struct fs_node *node);
-struct dirent *fs_readdir(struct fs_node *node);
-struct fs_node *fs_finddir(struct fs_node *node, char *name);
-int fs_create(struct fs_node *node, char *name, uint32_t flags);
-int fs_mkdir(struct fs_node *node, char *name, uint32_t flags);
-int fs_ioctl(struct fs_node *node, int req, void *argp);
-size_t fs_getsize(struct fs_node *node);
-int fs_chmod(struct fs_node *node, uint32_t mode);
-int fs_unlink(struct fs_node *node);
-int fs_symlink(struct fs_node *node, struct fs_node *link);
-int fs_readlink(struct fs_node *node);
+int inode_read(struct inode *node, size_t sz, int n, char *buf);
+int inode_write(struct inode *node, size_t sz, int n, char *buf);
+int inode_rewind(struct inode *node);
+int inode_open(struct inode *node, uint32_t flags);
+int inode_close(struct inode *node);
+struct dirent *inode_readdir(struct inode *node);
+struct inode *inode_finddir(struct inode *node, char *name);
+int inode_create(struct inode *node, char *name, uint32_t flags);
+int inode_mkdir(struct inode *node, char *name, uint32_t flags);
+int inode_ioctl(struct inode *node, int req, void *argp);
+size_t inode_getsize(struct inode *node);
+int inode_chmod(struct inode *node, uint32_t mode);
+int inode_unlink(struct inode *node);
+int inode_symlink(struct inode *node, struct inode *link);
+int inode_readlink(struct inode *node);
 
+/* Photon File System */
+#define PHFS_SUPER_MAGIC 0xABCDABCD
+#define PHFS_FILE_MAGIC 0xABCDBEEF
+#define PHFS_DEAD_MAGIC 0xDEADBEEF
+
+#define PHFS_FILE 0x1
+#define PHFS_DIRECTORY 0x2
+
+typedef struct phos_super_block {
+    uint32_t magic;
+    uint32_t length;
+    uint32_t free_space;
+    uint32_t c_inode;
+    uint32_t nfiles;
+} phos_super_block_t;
+
+typedef struct phos_file_header {
+    uint32_t magic;
+    uint32_t inode;
+    char name[30];
+    uint32_t type;
+    uint32_t offset;
+    uint32_t length;
+} phos_file_header_t;
+
+int phfs_read(struct inode *node, size_t sz, int n, char *buf);
+int phfs_write(struct inode *node, size_t sz, int n, char *buf);
+int phfs_open(struct inode *node, uint32_t flags);
+int phfs_close(struct inode *node);
+int phfs_rewind(struct inode *node);
 #endif
