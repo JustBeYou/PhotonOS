f6b9306ffc9d63bdec53675ca7bbcecc1943ed4f
JustBeYou <mihailferaru2000@gmail.com>
2016-01-14 17:01:20 +0200

    Changed functions' declarations and structure.

--- src/libc/include/graph.h
+++ src/libc/include/graph.h
@@ -8,18 +8,13 @@
 #include <sys/cdefs.h>
 
 typedef struct graph_node {
-    union {
-        int id;
-        char *name;
-    };
     int visited;
-    
     void *data;
     Llist_t *nodes;
 } graph_node_t;
 
-graph_node_t *graph_create(int id, char *name, void *data);
-graph_node_t *graph_find(graph_node_t *base, int id, char *name);
+graph_node_t *graph_create(void *data);
+graph_node_t *graph_find(graph_node_t *base);
 int graph_add_node(graph_node_t *parent, graph_node_t *child);
 int graph_is_reachable(graph_node_t *a, graph_node_t *b);
 void graph_destroy(graph_node_t *graph);
