8b63b158843f19eae219a2da6c5a5c4027549d07
JustBeYou <mihailferaru2000@gmail.com>
2016-02-13 15:44:05 +0200

    Moved usermode and syscalls functions.

--- src/kernel/system/process.c
+++ src/kernel/system/process.c
@@ -17,48 +17,11 @@
 /*** TODO: GET STABLE processING ***/
 
 extern int time_to_run;
+extern uint32_t user_stack;
 process_t *current_process;
 process_t *start_process;
 uint32_t pid;
-int start_processing = 0;
-
-void *syscalls[] = {
-	&write,
-    &read
-};
-int num_syscalls = 2;
-
-extern void kernel_main();
-extern void jmp_to_usermode();
-
-void init_usermode() {
-	set_kernel_stack((uint32_t) stack_top);
-    jmp_to_usermode();
-}
-
-void syscall_handler(registers_t *regs)
-{
-	if (regs->eax > (uint32_t) num_syscalls)
-		return;
-
-	void *location = syscalls[regs->eax - 1];
-
-	int ret;
-	asm volatile (" \
-		push %1; \
-		push %2; \
-		push %3; \
-		push %4; \
-		push %5; \
-		call *%6; \
-		pop %%ebx; \
-		pop %%ebx; \
-		pop %%ebx; \
-		pop %%ebx; \
-		pop %%ebx; \
-		" : "=a" (ret) : "r" (regs->edi), "r" (regs->esi), "r" (regs->edx), "r" (regs->ecx), "r" (regs->ebx), "r" (location));
-		regs->eax = ret;
-}
+int switch_on = 0;
 
 void init_multitasking()
 {
@@ -69,14 +32,25 @@ void init_multitasking()
     start_process = (process_t*) kmalloc(sizeof(process_t), 0, 0);
     start_process->pid = 1;
     start_process->time_to_run = 100;
+    start_process->ready = 1;
     start_process->prior = 0;
-    memcpy(start_process->name, "kernel", 7);
+    memcpy(start_process->name, "kernel\0", 7);
     start_process->next = NULL;
-    time_to_run = 10;
+
+    start_process->esp = 0;
+    start_process->ss = 0x13;
+    start_process->kern_stack = kmalloc(PROC_KERN_STACK, 1, 0) + PROC_KERN_STACK;
+    start_process->user_stack = kmalloc(PROC_USER_STACK, 1, 0) + PROC_USER_STACK;
+    user_stack = start_process->user_stack;
+    set_kernel_stack(start_process->kern_stack);
+    start_process->cr3 = 0;
+
+    time_to_run = 0;
 
 	current_process = start_process;
 }
 
+/* Useless until there is a file system. */
 process_t *create_process(char *name)
 {
     uint32_t cr3 = read_cr3();
@@ -84,9 +58,12 @@ process_t *create_process(char *name)
 	process_t *new_process = (process_t*) kmalloc(sizeof(process_t), 0, 0);
 	new_process->pid = pid;
     new_process->time_to_run = 10;
+    new_process->ready = 1;
     new_process->prior = 0;
     memcpy(new_process->name, name, strlen(name) + 1);
 	new_process->next = NULL;
+
+    /* Here I should load the file and create a context for the new process. */
 
 	if (pid != (uint32_t) 1) {
 		process_t *tmp_process = start_process;
