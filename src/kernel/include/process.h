59e91d05f2b8745dae51b11da9400d3d85d9e742
JustBeYou <mihailferaru2000@gmail.com>
2016-02-13 15:44:17 +0200

    Improved process_t.

--- src/kernel/include/process.h
+++ src/kernel/include/process.h
@@ -7,24 +7,23 @@
 #include <string.h>
 #include <phapi.h>
 
+#define PROC_USER_STACK 0x200000 // 2 MiB
+#define PROC_KERN_STACK 0x80000  // 512 KiB
+#define PROC_USER_HEAP  0x80000  // 512 Kib (initial size, can be expanded)
+
 typedef struct process {
-    uint32_t pid;
-    uint32_t time_to_run;
-    uint8_t ready;
-    uint8_t prior;
-    char name[32];
-    struct process *next;
+    uint32_t pid; // ID
+    uint32_t esp; // current kernel stack position
+    uint32_t ss; // current kernel stack segment
+    uint32_t kern_stack; // where kernel stack is allocated
+    uint32_t user_stack; // where user stack is allocated
+    uint32_t cr3; // current page directory
+    uint32_t time_to_run; // time allocated for process
+    uint32_t ready; // ready or not?
+    uint32_t prior; // priority
+    char name[32]; // name of process
+    struct process *next; // next process in list
 } process_t;
-
-/**
-  * Init user mode.
-  */
-void init_usermode();
-
-/**
-  * System calls handler.
-  */
-void syscall_handler(registers_t *regs);
 
 /**
   * Initialize processing.
