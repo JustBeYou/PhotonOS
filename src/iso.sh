0beca4bd3491e9ee8ff398ccd2311ac8193760d2
JustBeYou <mihailferaru2000@gmail.com>
2016-02-17 15:34:02 +0200

    Added initrd module.

--- src/iso.sh
+++ src/iso.sh
@@ -3,15 +3,16 @@ set -e
 . ./build.sh
 
 mkdir -p isodir
+mkdir -p isodir/usr
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/photon.elf isodir/boot/photon.elf
-tar --verbose --create --file isodir/boot/photon.initrd --directory=sysroot $(ls sysroot | grep -v boot)
+cp -r sysroot/usr/* isodir/usr
+cp -r sysroot/boot/* isodir/boot/
 cat > isodir/boot/grub/grub.cfg << EOF
 menuentry "photon" {
 	multiboot /boot/photon.elf
-	module /boot/photon.initrd
+    module /boot/initrd
 }
 EOF
 grub-mkrescue -o photon.iso isodir
