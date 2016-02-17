f2cc2ffa5e72b8d014bb9486f706c14a0592aaac
JustBeYou <mihailferaru2000@gmail.com>
2016-02-17 15:35:47 +0200

    Removed Photon File System.

--- src/kernel/include/vfs.h
+++ src/kernel/include/vfs.h
@@ -62,11 +62,11 @@ typedef struct inode {
     uint32_t open_flags;  // Flags for open
     uint32_t lock;        // Inode is locked or not
     size_t offset;        // Cursor position
-    
+
     uint32_t atime;       // Accessed time
     uint32_t mtime;       // Modified time
     uint32_t ctime;       // Created time
-    
+
     // Functions
     inode_read_t      read;
     inode_write_t     write;
@@ -83,7 +83,7 @@ typedef struct inode {
     inode_unlink_t    unlink;
     inode_symlink_t   symlink;
     inode_readlink_t  readlink;
-    
+
     // Link and reference
     struct inode *link;
     int32_t refcount;
@@ -100,7 +100,7 @@ typedef struct super_block {
     uint32_t block;
     file_system_type *fs_type;
     void *fs_data;
-    
+
     // TODO: add operations
 } super_block_t;
 
@@ -142,35 +142,4 @@ int inode_chmod(struct inode *node, uint32_t mode);
 int inode_unlink(struct inode *node);
 int inode_symlink(struct inode *node, struct inode *link);
 int inode_readlink(struct inode *node);
-
-/* Photon File System */
-#define PHFS_SUPER_MAGIC 0xABCDABCD
-#define PHFS_FILE_MAGIC 0xABCDBEEF
-#define PHFS_DEAD_MAGIC 0xDEADBEEF
-
-#define PHFS_FILE 0x1
-#define PHFS_DIRECTORY 0x2
-
-typedef struct phos_super_block {
-    uint32_t magic;
-    uint32_t length;
-    uint32_t free_space;
-    uint32_t c_inode;
-    uint32_t nfiles;
-} phos_super_block_t;
-
-typedef struct phos_file_header {
-    uint32_t magic;
-    uint32_t inode;
-    char name[30];
-    uint32_t type;
-    uint32_t offset;
-    uint32_t length;
-} phos_file_header_t;
-
-int phfs_read(struct inode *node, size_t sz, int n, char *buf);
-int phfs_write(struct inode *node, size_t sz, int n, char *buf);
-int phfs_open(struct inode *node, uint32_t flags);
-int phfs_close(struct inode *node);
-int phfs_rewind(struct inode *node);
 #endif
--- src/kernel/system/vfs.c
+++ src/kernel/system/vfs.c
@@ -20,62 +20,17 @@ void init_vfs()
 {
     for (int i = 0; i < MAX_KERNEL_FD; i++) {
         kernel_fd[i] = NULL;
-    } 
-    
+    }
+
     /* Virtual Disk Init */
-    virtual_disk = calloc(0x5000);
-    phos_super_block_t *vd_super_block = calloc(sizeof(phos_super_block_t));
-    phos_file_header_t *vd_root_file = calloc(sizeof(phos_file_header_t));
     
-    vd_super_block->magic = PHFS_SUPER_MAGIC;
-    vd_super_block->length = 0x5000;
-    vd_super_block->free_space = 0x5000;
-    vd_super_block->c_inode = 0;
-    vd_super_block->nfiles = 0;
-    
-    vd_root_file->magic = PHFS_FILE_MAGIC;
-    vd_root_file->inode = 0;
-    memcpy(vd_root_file->name, "root\0", 5);
-    vd_root_file->type = PHFS_FILE;
-    vd_root_file->offset = sizeof(phos_super_block_t);
-    vd_root_file->length = 0;
-    
-    memcpy(virtual_disk, vd_super_block, sizeof(phos_super_block_t));
-    memcpy(virtual_disk + sizeof(phos_super_block_t), vd_root_file, sizeof(phos_file_header_t));
-    
-    /* Virtual File System Init */
-    vfs_root = graph_create(NULL);
-    vfs_super_block = NULL; // TODO: implement super block
-    vfs_root_inode = malloc(sizeof(inode_t));
-    inode_cache = Llist_create();
-    directory_cache = Llist_create();
-    
-    vfs_root->data = (void*) vfs_root_inode;
-    
-    vfs_root_inode->device = (void*) virtual_disk;
-    vfs_root_inode->mask = 0;
-    vfs_root_inode->uid = 0;
-    vfs_root_inode->gid = 0;
-    vfs_root_inode->flags = FS_DIRECTORY;
-    vfs_root_inode->inode = 0;
-    vfs_root_inode->length = 0;
-    vfs_root_inode->block = 4096;
-    vfs_root_inode->open_flags = 0;
-    vfs_root_inode->lock = 0;
-    vfs_root_inode->offset = 0;
-    vfs_root_inode->atime = 0;
-    vfs_root_inode->mtime = 0;
-    vfs_root_inode->ctime = 0;
-    vfs_root_inode->link = NULL;
-    vfs_root_inode->refcount = 1;
-    vfs_root_inode->nlink = 0;
 }
 
 int inode_read(inode_t *node, size_t sz, int n, char *buf)
 {
     if (!node)
         return -1;
-        
+
     if (node->read) {
         int ret = node->read(node, sz, n, buf);
         return ret;
@@ -89,12 +44,12 @@ int inode_write(inode_t *node, size_t sz, int n, char *buf)
 {
     if (!node)
         return -1;
-        
+
     if (node->write) {
         int ret = node->write(node, sz, n, buf);
         return ret;
     }
-    
+
     return -1;
 }
 
@@ -102,7 +57,7 @@ int inode_rewind(inode_t *node)
 {
     if (!node)
         return -1;
-        
+
     node->rewind(node);
     return 0;
 }
@@ -111,16 +66,16 @@ int inode_open(inode_t *node, uint32_t flags)
 {
     if (!node)
         return -1;
-    
+
     if (node->refcount >= 0) {
         node->refcount++;
     }
-    
+
     if (node->open) {
         int ret = node->open(node, flags);
         return ret;
     }
-    
+
     return -1;
 }
 
@@ -130,15 +85,15 @@ int inode_close(inode_t *node)
         printk("[WARN] Attempt to close the root.\n");
         return -1;
     }
-    
+
     if (!node) {
         return -1;
     }
-    
+
     if (node->refcount == -1) {
         return -1;
     }
-    
+
     node->refcount--;
     if (node->refcount == 0) {
         if (node->close) {
@@ -197,30 +152,4 @@ int inode_symlink(inode_t *node, struct inode *link)
 int inode_readlink(inode_t *node)
 {
     return 0;
-}
-
-/* Photon File System */
-int phfs_read(struct inode *node, size_t sz, int n, char *buf)
-{
-
-}
-
-int phfs_write(struct inode *node, size_t sz, int n, char *buf)
-{
-
-}
-
-int phfs_open(struct inode *node, uint32_t flags)
-{
-
-}
-
-int phfs_close(struct inode *node)
-{
-
-}
-
-int phfs_rewind(struct inode *node)
-{
-
 }
