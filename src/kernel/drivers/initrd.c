fac51f40e1ac5cc73e35ac8abcaddbb4500f79b6
JustBeYou <mihailferaru2000@gmail.com>
2016-02-17 15:36:36 +0200

    Partial initrd driver.

---
+++ src/kernel/drivers/initrd.c
@@ -0,0 +1,26 @@
+#include <system.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <phapi.h>
+
+/* Initrd File System */
+int initrd_read(struct inode *node, size_t sz, int n, char *buf)
+{
+
+}
+
+int initrd_open(struct inode *node, uint32_t flags)
+{
+
+}
+
+int initrd_close(struct inode *node)
+{
+
+}
+
+int initrd_rewind(struct inode *node)
+{
+
+}
