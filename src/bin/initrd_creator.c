f0486473db2e837be8ad6e71832e514fa6d6fb45
JustBeYou <mihailferaru2000@gmail.com>
2016-03-04 20:58:15 +0200

    Created root directory for initrd.

--- src/bin/initrd_creator.c
+++ src/bin/initrd_creator.c
@@ -9,14 +9,25 @@
 
 #define INITRD_MAGIC 0xBEEFBEEF
 
+#define FS_FILE       0x01
+#define FS_DIRECTORY  0x02
+#define FS_DEVICE     0x04
+#define FS_PIPE       0x08
+#define FS_SYMLINK    0x10
+#define FS_MOUNTPOINT 0x20
+#define FS_META       0x40
+
 typedef struct initrd_super_block {
     uint32_t magic;
     uint32_t sb_struct_size;
     uint32_t fh_struct_size;
+    uint32_t files;
 } initrd_super_block_t;
 
 typedef struct initrd_file_header {
+    uint32_t magic;
     uint32_t inode;
+    uint32_t flags;
     char name[32];
     uint32_t length;
 } initrd_file_header_t;
@@ -28,9 +39,24 @@ int main(int argc, char **argv)
     sb->magic = INITRD_MAGIC;
     sb->sb_struct_size = sizeof(initrd_super_block_t);
     sb->fh_struct_size = sizeof(initrd_file_header_t);
+    sb->files = argc;
     fwrite(sb, sb->sb_struct_size, 1, initrd);
 
     int inode = 0;
+
+    initrd_file_header_t *root_dir = malloc(sizeof(initrd_file_header_t));
+    root_dir->magic = INITRD_MAGIC;
+    root_dir->inode = inode++;
+    root_dir->flags = FS_DIRECTORY;
+    memset(root_dir->name, 0, 32);
+    strcpy(root_dir->name, "/");
+    root_dir->length = sizeof(int) * (argc - 1);
+    fwrite(root_dir, sb->fh_struct_size, 1, initrd);
+    for (int i = 0, tmp = inode; i < argc - 1; i++) {
+        fwrite((void*) &tmp, sizeof(int), 1, initrd);
+        tmp++;
+    }
+
     for (int i = 1; i < argc; i++) {
         FILE *f = fopen(argv[i], "rb");
         fseek(f, 0L, SEEK_END);
@@ -38,7 +64,10 @@ int main(int argc, char **argv)
         fseek(f, 0L, SEEK_SET);
 
         initrd_file_header_t *fh = malloc(sizeof(initrd_file_header_t));
+        fh->magic = INITRD_MAGIC;
+        fh->flags = FS_FILE;
         fh->inode = inode++;
+        memset(fh->name, 0, 32);
         strcpy(fh->name, argv[i]);
         fh->length = sz;
 
@@ -54,6 +83,7 @@ int main(int argc, char **argv)
     }
 
     free(sb);
+    free(root_dir);
     fclose(initrd);
 
     return 0;
