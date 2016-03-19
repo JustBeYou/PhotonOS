f45ed13b3f80917bdb6454da08c8ad1475f626f9
JustBeYou <mihailferaru2000@gmail.com>
2016-03-19 15:26:26 +0200

    Removed debug info.

--- src/fs/initrd.c
+++ src/fs/initrd.c
@@ -12,19 +12,21 @@
 
 initrd_super_block_t *sb;
 initrd_file_header_t *fheader;
+initrd_file_header_t *initrd_headers;
 inode_t *initrd_nodes;
 
 /* Initrd File System */
 void initrd_load(uint32_t location)
 {
-    printk("\n\n---INITRD TEST---\n");
     sb = (initrd_super_block_t*) location;
-    fheader = kmalloc(sizeof(initrd_file_header_t), 0, 0);
+    fheader = NULL;
     initrd_nodes = kmalloc(sizeof(inode_t) * sb->files, 0, 0);
+    initrd_headers = kmalloc(sizeof(initrd_file_header_t) * sb->files, 0, 0);
 
     location += sb->sb_struct_size;
     for (int i = 0; i < sb->files; i++) {
         fheader = (initrd_file_header_t*) location;
+        memcpy(&(initrd_headers[i]), fheader, sizeof(initrd_file_header_t));
         uint32_t block_addr = (uint32_t) fheader + sb->fh_struct_size;
         inode_init(&initrd_nodes[i], fheader->flags, fheader->inode,
                     fheader->length, 0, 0, block_addr);
@@ -33,22 +35,9 @@ void initrd_load(uint32_t location)
         initrd_nodes[i].rewind = &initrd_rewind;
         initrd_nodes[i].close = &initrd_close;
 
-        printk("%x %x %x\n", initrd_nodes[i].inode, initrd_nodes[i].flags, initrd_nodes[i].length);
-
         location += (uint32_t) sizeof(initrd_file_header_t);
         location += (uint32_t) fheader->length;
     }
-
-    char buf[512];
-    memset(buf, 0, 512);
-    inode_read(&initrd_nodes[1], 10, 1, buf);
-    printk("%s\n", buf);
-    memset(buf, 0, 512);
-    inode_rewind(&initrd_nodes[1]);
-    inode_read(&initrd_nodes[1], 10, 4, buf);
-    printk("%s\n", buf);
-
-    printk("-----------------");
 }
 
 int initrd_read(struct inode *node, size_t sz, int n, char *buf)
