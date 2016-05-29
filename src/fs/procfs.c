6cf0328c06311d411b93f7f6cfa253cbb463d479
JustBeYou <mihailferaru2000@gmail.com>
2016-05-29 12:53:20 +0300

    Added file creation procedure.

--- src/fs/procfs.c
+++ src/fs/procfs.c
@@ -12,6 +12,9 @@
 int inode_id;
 procfs_super_block_t procfs_sb;
 
+extern char cwd[4096];
+extern struct dentry *cwd_de;
+
 void procfs_init()
 {
     inode_id = 0;
@@ -19,16 +22,17 @@ void procfs_init()
     procfs_sb.fh_struct_size = sizeof(procfs_file_header_t);
 }
 
-struct inode *procfs_create(uint32_t flags, uint32_t length)
+struct inode *procfs_alloc_inode(uint32_t flags, uint32_t length)
 {
     struct inode *node = kmalloc(sizeof(struct inode), 0, 0);
-    inode_init(node, flags, inode_id++, length, 0, 0, 0);
+    inode_init(node, flags, inode_id++, length, 0, 0, 0, PROCFS_TYPE);
     procfs_alloc(node, length);
     node->write = &procfs_write;
     node->read = &procfs_read;
     node->rewind = &procfs_rewind;
     node->open = &procfs_open;
     node->close = &procfs_close;
+    node->create = &procfs_create;
 
     return node;
 }
@@ -48,6 +52,43 @@ void procfs_alloc(struct inode *node, uint32_t length)
     }
     procfs_file_header_t *addr = (procfs_file_header_t*) node->block;
     addr->length = length;
+}
+
+int procfs_create(struct inode *parent, char *name, uint32_t flags)
+{
+    struct dentry *parent_de = get_dentry_by_inode(parent);
+    if (parent_de == NULL) {
+        return -1;
+    }
+
+    inode_t *new_inode = procfs_alloc_inode(flags, 1024);
+    struct dentry *new_de = kmalloc(sizeof(struct dentry), 0, 0);
+
+    size_t namelen = strlen(name);
+    new_de->name = kmalloc(sizeof(char) * namelen + 1, 0, 0);
+    memcpy(new_de->name, name, namelen);
+    new_de->name[namelen] = '\0';
+
+    new_de->parent_dentry = parent_de;
+    new_de->inode = new_inode;
+    add_dentry(new_de);
+
+    uint32_t addr = &new_de;
+    inode_rewind(parent);
+    uint32_t temp_addr;
+    inode_read(parent, sizeof(uint32_t), 1, (char*) temp_addr);
+    while (temp_addr != 0x0) {
+        inode_read(parent, sizeof(uint32_t), 1, (char*) temp_addr);
+    }
+    parent->offset += sizeof(uint32_t);
+
+    inode_write(parent, sizeof(uint32_t), 1, (char*) addr);
+
+    graph_node_t *new_node = graph_create((void*) new_de);
+    graph_node_t *parent_node = get_node_by_path(cwd);
+    graph_add_node(parent_node, new_node);
+
+    return 0;
 }
 
 int procfs_write(struct inode *node, size_t sz, int n, char *buf)
