7df77202c29f801eb603d85f3489497a907a8868
JustBeYou <mihailferaru2000@gmail.com>
2019-12-06 14:06:40 +0200

    Changed guards

--- src/kernel/vga.c
+++ src/kernel/vga.c
@@ -17,7 +17,7 @@
 #include <kernel/io.h>
 
 uint16_t *vga_memory;
-#ifdef _TEXTMODE
+
 int tabstop;
 size_t row;
 size_t col;
@@ -25,11 +25,11 @@ enum vga_color default_bg = COLOR_BLACK;
 enum vga_color default_fg = COLOR_LIGHT_GREY;
 static const size_t VGA_WIDTH = 80;
 static const size_t VGA_HEIGHT = 25;
-#endif
+
 
 extern void cmd_dbg();
 
-#ifdef _TEXTMODE
+
 uint8_t make_color(enum vga_color fg, enum vga_color bg)
 {
     return fg | bg << 4;
@@ -41,11 +41,11 @@ uint16_t make_vgaentry(char c, uint8_t color)
     uint16_t color16 = color;
     return c16 | color16 << 8;
 }
-#endif
+
 
 void init_vga()
 {
-    #ifdef _TEXTMODE
+    
     vga_memory = (uint16_t*) 0xB8000;
     row = 0;
     col = 0;
@@ -53,12 +53,12 @@ void init_vga()
     default_bg = COLOR_BLACK;
     default_fg = COLOR_LIGHT_GREY;
     clear_vga();
-    #endif
+    
 }
 
 void clear_vga()
 {
-    #ifdef _TEXTMODE
+    
     for ( size_t y = 0; y < VGA_HEIGHT; y++ )
     {
         for ( size_t x = 0; x < VGA_WIDTH; x++ )
@@ -70,12 +70,12 @@ void clear_vga()
     row = 0;
     col = 0;
     vga_move_cursor(row, col);
-    #endif
+    
 }
 
 int vga_scroll(size_t *row)
 {
-    #ifdef _TEXTMODE
+    
     uint16_t blank = make_vgaentry(' ', make_color(default_fg, default_bg));
 
     if (*row >= 25)
@@ -88,10 +88,10 @@ int vga_scroll(size_t *row)
     }
     vga_move_cursor(*row, col);
     return 0;
-    #endif
+    
 }
 
-#ifdef _TEXTMODE
+
 void vga_setcolor(enum vga_color fg, enum vga_color bg)
 {
     default_bg = bg;
@@ -103,11 +103,11 @@ void vga_putentryat(char c, uint8_t color, size_t x, size_t y)
     const size_t index = y * VGA_WIDTH + x;
     ((uint16_t*) vga_memory)[index] = make_vgaentry(c, color);
 }
-#endif
+
 
 void vga_putchar(char c)
 {
-    #ifdef _TEXTMODE
+    
     switch (c) {
         /**
          * \n  - new line
@@ -157,19 +157,19 @@ void vga_putchar(char c)
     }
     vga_scroll(&row);
     vga_move_cursor(row, col + 1);
-    #endif
+    
 }
 
 void vga_writestring(const char* data)
 {
-    #ifdef _TEXTMODE
+    
     size_t datalen = strlen(data);
     for ( size_t i = 0; i < datalen; i++ )
         vga_putchar(data[i]);
-    #endif
+    
 }
 
-#ifdef _TEXTMODE
+
 void vga_set_tab(int size)
 {
     tabstop = size;
@@ -183,26 +183,25 @@ void vga_move_cursor(int row, int column)
     outb(0x3D4, 15);
     outb(0x3D5, cursor_pos);
 }
-#endif
+
 
 void vga_putchar_color(char c, enum vga_color fg)
 {
-    #ifdef _TEXTMODE
+    
     uint8_t temp_color = default_fg;
     default_fg = fg;
     vga_putchar(c);
     default_fg = temp_color;
-    #endif
+    
 }
 
 void wstr_color(const char* data, enum vga_color fg)
 {
-    #ifdef _TEXTMODE
+// guards because I directly call this in the main function XD
+#if defined(_TEXTMODE)
     for ( size_t i = 0, n = strlen(data); i < n; i++ )
         vga_putchar_color((int) ((const unsigned char*) data)[i], fg);
-    #endif
-}
-
-#ifndef _TEXTMODE
-#error "Graphic Mode not ready!"
+#else
+    putsk(data);
 #endif
+}
